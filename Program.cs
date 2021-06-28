fcf29456cd99cc559003d99260461c625ffd7e83
Tamzid Raihan <tamzidraihan7@gmail.com>
2021-06-28 23:50:47 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProblemSolving01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Id = int.Parse(Console.ReadLine());
+            int workedHours = int.Parse(Console.ReadLine());
+            double paymentPerHour = double.Parse(Console.ReadLine());
+
+            double salary = workedHours * paymentPerHour;
+
+            Console.WriteLine("NUMBER = {0}\nSALARY = U$ {1:0.00}", Id, salary);
+        }
+    }
+}
